861faff20f0d9e43ad467b65fa47c6fc848814df
wdmomoxx <540825306@qq.com>
2019-06-06 22:39:40 +0800

    Update install_omv.sh

--- install_omv.sh
+++ install_omv.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-#OS=`tail -n 1 /etc/lsb-release | awk -F' ' 'NR==1{print $2}'`
-#kernel=`uname -r`
-#user=`whoami`
-#aptfast="/usr/local/sbin/apt-fast"
+OS=`tail -n 1 /etc/lsb-release | awk -F' ' 'NR==1{print $2}'`
+kernel=`uname -r`
+user=`whoami`
+aptfast="/usr/local/sbin/apt-fast"
 #if [ "$OS" != "16.04.6" ] || [ "$kernel" != "4.4.0-131-generic" ];then
-#if [ "$OS" != "16.04.3" ] || [ "$kernel" != "4.3.3.001+" ];then
-#    echo "您的系统不是官方原装ubuntu16.04系统, 无法安装，请更新到最新的官方系统之后再次运行本脚本。"
-#    echo "更新到最新的官方系统的方法如下："
-#    echo "下载群附件《WISNUC系统镜像（请解压后烧录安装）》并写入U盘。"
-#    echo "然后将U盘插入Wisnuc，启动之后自动升级系统，详情请参考群文件《WISNUC系统20170930版本安装说明》。"
-#    exit
-#fi
-#if [ "$user" != "root" ];then
-#    echo "您不是root用户，无法安装，即将退出。"
-#    echo "请在本脚本退出后执行命令sudo su之后再次运行本脚本。"
-#    exit
-#fi
+if [ "$OS" != "16.04.3" ] || [ "$kernel" != "4.3.3.001+" ];then
+    echo "您的系统不是官方原装ubuntu16.04系统, 无法安装，请更新到最新的官方系统之后再次运行本脚本。"
+    echo "更新到最新的官方系统的方法如下："
+    echo "下载群附件《WISNUC系统镜像（请解压后烧录安装）》并写入U盘。"
+    echo "然后将U盘插入Wisnuc，启动之后自动升级系统，详情请参考群文件《WISNUC系统20170930版本安装说明》。"
+    exit
+fi
+if [ "$user" != "root" ];then
+    echo "您不是root用户，无法安装，即将退出。"
+    echo "请在本脚本退出后执行命令sudo su之后再次运行本脚本。"
+    exit
+fi
 
 echo
 echo "即将安装apt-fast以加快之后的安装速度。"
 apt install -y curl
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
 
-echo
-echo "下面开始设置root用户的密码，请输入您想要设置的密码后请回车，系统会提示您再次输入密码以确认。"
-passwd root
+#echo
+#echo "下面开始设置root用户的密码，请输入您想要设置的密码后请回车，系统会提示您再次输入密码以确认。"
+#passwd root
 
 echo
 echo "接下来开始新建openmediavault.list，并添加omv的源。此操作可能需要花十分钟时间，请耐心等待。"
