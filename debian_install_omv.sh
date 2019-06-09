82e1362caa42922aa0ad4f38bcf628b1fd25561b
wdmomoxx <540825306@qq.com>
2019-06-09 13:18:16 +0800

    Create debian_install_omv.sh

---
+++ debian_install_omv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#install omv on debian stretch
+
+echo "deb http://packages.openmediavault.org/public arrakis main" > /etc/apt/sources.list.d/openmediavault.list
+
+apt-get update
+
+apt-get --allow-unauthenticated install openmediavault-keyring
+
+apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option Dpkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install postfix openmediavault
